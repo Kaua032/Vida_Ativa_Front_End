db641a9adf1cd6a025bfa50295638d4ac78678c6
Kauã Xavier <kauaxavier032@gmail.com>
2023-12-20 03:30:10 -0300

    feat: Update codebase to use React Router for page routing.

--- Vida_Ativa/src/main.jsx
+++ Vida_Ativa/src/main.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { GlobalStyled } from "./GlobalStyled.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Pages/Login/Login.jsx";
+import Home from "./Pages/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyled />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
