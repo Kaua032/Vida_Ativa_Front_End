17b610a314424a13a23106d5ebebcc368912e57c
Kauã Xavier <kauaxavier032@gmail.com>
2024-01-11 16:18:16 -0300

    feat: Add buttons to the home page

--- Vida_Ativa/src/Pages/Home/Home.jsx
+++ Vida_Ativa/src/Pages/Home/Home.jsx
@@ -1,6 +1,9 @@
+import DefaultButton from "../../components/Button/DefaultButton";
 import Graphic from "../../components/Graphic/Graphic";
 import Navbar from "../../components/Navbar/Navbar";
 import { MainHome } from "./HomeStyled";
+import { Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
 function Home() {
   return (
@@ -8,6 +11,11 @@ function Home() {
       <Navbar type1="1" type2="2" state1="on" state2="off" />
       <div id="painel">
         <Graphic />
+        <div id="painel_buttons">
+          <Button variant="primary"  className="new-student">Novo Aluno</Button>
+          <DefaultButton img="./plusIcon.png" text="Nova Frequência" />
+          <DefaultButton img="./searchIcon.png" text="Consultar Frequência" />
+        </div>
       </div>
     </MainHome>
   );
--- Vida_Ativa/src/Pages/Home/HomeStyled.jsx
+++ Vida_Ativa/src/Pages/Home/HomeStyled.jsx
@@ -1,14 +1,42 @@
 import styled from "styled-components";
 
 export const MainHome = styled.main`
+  display: flex;
+
+  #painel {
+    width: 100%;
     display: flex;
+    flex-direction: column;
+    align-items: center;
 
-    #painel{
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    padding: 50px;
+  }
+  #painel_buttons {
+    width: 630px;
+    display: flex;
+    justify-content: space-between;
+    margin: auto;
+  }
+  #painel_buttons .new-student {
+    height: 40px;
+    padding: 0px 15px 0px 45px;
 
-        padding: 50px;
-    }
-`
+    border-radius: 50px;
+    background: #0a7de8;
+
+    background-image: url("./plusIcon.png");
+    background-repeat: no-repeat;
+    background-position: 5px 4px;
+
+    border: none;
+
+    color: #fff;
+    font-family: Montserrat;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+
+    cursor: pointer;
+  }
+`;
