f749a2626d49df64b247d72db355a4c2ee2ce499
Kauã Xavier <kauaxavier032@gmail.com>
2024-01-29 22:43:45 -0300

    feat: Add private route for login page

--- Vida_Ativa/src/main.jsx
+++ Vida_Ativa/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { GlobalStyled } from "./GlobalStyled.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -6,11 +6,30 @@ import Login from "./Pages/Login/Login.jsx";
 import Home from "./Pages/Home/Home.jsx";
 import UserProvider from "./Context/UserContext.jsx";
 import Teachers from "./Pages/Teachers/Teachers.jsx";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
+function PrivateRoute({ children }) {
+  let navigate = useNavigate();
+  let token = Cookies.get("token");
+
+  useEffect(() => {
+    if (token) {
+      navigate("/home");
+    }
+  }, [navigate, token]);
+
+  return token ? null : children;
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: (
+      <PrivateRoute>
+        <Login />,
+      </PrivateRoute>
+    ),
   },
   {
     path: "/home",
